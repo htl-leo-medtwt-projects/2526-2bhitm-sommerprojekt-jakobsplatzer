a9b89d6dec9d5b7d6d368f9097201b9c13bb9958
jakobsplatzer <j.platzer@students.htl-leonding.ac.at>
2026-04-08 14:32:58 +0200

    Add navigation buttons and update script functions for content transitions

--- project/script.js
+++ project/script.js
@@ -1,9 +1,16 @@
 let startSection = document.getElementById('start-section');
 let videoSection = document.getElementById('video-section');
 let conWithChick1 = document.getElementById('con-with-chick1');
+let conWithChick2 = document.getElementById('con-with-chick2');
+let conWithChick3 = document.getElementById('con-with-chick3');
+let conWithChick4 = document.getElementById('con-with-chick4');
+
 
 videoSection.style.display = 'none';
 conWithChick1.style.display = 'none';
+conWithChick2.style.display = 'none';
+conWithChick3.style.display = 'none';
+conWithChick4.style.display = 'none';
 
 
 
@@ -16,3 +23,18 @@ function vidToCon(){
     videoSection.style.display = 'none';
     conWithChick1.style.display = '';
 }
+
+function Con1ToCon2(){
+    conWithChick1.style.display = 'none';
+    conWithChick2.style.display = '';
+}
+function Con2ToCon3(){
+    conWithChick2.style.display = 'none';
+    conWithChick3.style.display = '';
+}
+function Con3ToCon4(){
+    conWithChick3.style.display = 'none';
+    conWithChick4.style.display = '';
+}
+
+
